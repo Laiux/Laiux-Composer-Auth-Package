4b17d558eae5d760786fb6d50bdd573ce97d69e7
Jorge Luis Calle Morales <jcalle@unifiedsoftware.us>
2024-01-11 20:24:03 -0500

    first commit

---
+++ LICENSE.md
@@ -0,0 +1,13 @@
+Licencia de Software Propietario
+
+Este software es propiedad exclusiva de LAIUX E.I.R.L. Se concede un derecho limitado y no exclusivo para utilizar el software solo en su forma original, sin modificar, con el propósito de uso interno y/o uso por parte del personal de la empresa.
+
+No se permite la modificación, ingeniería inversa, descompilación ni cualquier otro intento de acceder o modificar el código fuente de este software. Cualquier intento no autorizado de modificar, distribuir o utilizar de otra manera este software violará los derechos de propiedad intelectual de LAIUX E.I.R.L. y dará lugar a la terminación inmediata de esta licencia.
+
+Esta licencia no otorga ningún derecho sobre la distribución, sublicencia, venta o cualquier otra forma de transferencia de este software a terceros.
+
+Esta licencia estará vigente hasta que sea terminada por LAIUX E.I.R.L. LAIUX E.I.R.L. se reserva el derecho de terminar esta licencia en cualquier momento si se violan sus términos.
+
+TODOS LOS DERECHOS NO EXPRESAMENTE OTORGADOS EN ESTA LICENCIA ESTÁN RESERVADOS A LAIUX E.I.R.L.
+
+Fecha: 11/01/2024
---
+++ src/AuthenticationProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Laiux\Auth;
+
+use Firebase\JWT\JWT;
+use Illuminate\Support\ServiceProvider;
+
+class AuthenticationProvider extends ServiceProvider {
+    
+    public function boot(): void {
+        // ...
+    }
+
+    /**
+         * Generate a JWT with an object adding params for the generation
+         *
+         * @param object          $payload PHP Object
+         * @param string          $aud The Audience URL.
+         *
+         * @return string A signed JWT
+    */
+    public function generateJWTToken(object $payload, string $aud = null): string {
+        $data_object = (array) $payload;
+        $secret = config('auth.secret');
+        $iss = config('app.url');
+        $exp = config('auth.expiration_time');
+        $alg = config('auth.algorithm');
+
+        /*  ENGLISH REFERENCE
+            sub -> Subject: whom the token refers to.
+            iss -> Issuer: who created and signed this token.
+            aud -> Audience: who or what the token is intended for.
+            exp -> Expiration Time: Identifies the expiration time on or after which the JWT MUST NOT be accepted for processing.
+            iat -> Issued at: seconds since Unix epoch.
+            nbf -> Not valid Before: seconds since Unix epoch. */
+
+        /* SPANISH REFERENCE
+            sub -> Asunto: a quién se refiere el token.
+            iss -> Emisor: quién creó y firmó este token.
+            aud -> Audiencia: a quién o para qué está destinado el token.
+            exp -> Hora de vencimiento: identifica la hora de vencimiento a partir de la cual el JWT NO DEBE aceptarse para su procesamiento.
+            iat -> Emitido a las: segundos desde la época de Unix.
+            nbf -> No válido Antes: segundos desde la época de Unix. */
+
+        $issued_date = time();
+
+        $data_object['iat'] = $issued_date;
+        $data_object['exp'] = $issued_date+$exp;
+        if($iss) $data_object['iss'] = $iss;
+        if($aud) $data_object['aud'] = $aud;
+
+        $token = JWT::encode($data_object, $secret, $alg);
+
+        return $token;
+    }
+}
