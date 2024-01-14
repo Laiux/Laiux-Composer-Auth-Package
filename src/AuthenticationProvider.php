281402a9b71cfff59393fcf609cbebc69297f754
Jorge Luis Calle Morales <jcalle@unifiedsoftware.us>
2024-01-13 23:43:17 -0500

    Fix errors in the migrations config

--- src/AuthenticationProvider.php
+++ src/AuthenticationProvider.php
@@ -2,6 +2,8 @@
 
 namespace Laiux\Auth;
 
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class AuthenticationProvider extends ServiceProvider {
@@ -13,12 +15,21 @@ class AuthenticationProvider extends ServiceProvider {
     private function loadMigrations(): void {
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
 
-        $this->publishes(
-            [
-                __DIR__ . '/database/migrations' => base_path('database/migrations'),
-            ],
-            'laiux-auth-migrations'
-        );
+        $this->publishes([
+            __DIR__.'/../database/migrations/create_sessions_table.php.stub' => $this->getMigrationFileName('create_sessions_table.php'),
+        ], 'laiux-auth-migrations');
+    }
+
+    private function getMigrationFileName(string $migrationFileName): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        $filesystem = $this->app->make(Filesystem::class);
+
+        return Collection::make([$this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR])
+            ->flatMap(fn ($path) => $filesystem->glob($path.'*_'.$migrationFileName))
+            ->push($this->app->databasePath()."/migrations/{$timestamp}_{$migrationFileName}")
+            ->first();
     }
 
 }
