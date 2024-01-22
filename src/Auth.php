e274f690e70b3f9063cde4cd2601fe4fe731afc3
Jorge Luis Calle Morales <jcalle@unifiedsoftware.us>
2024-01-21 19:45:46 -0500

    Complete the function for use the login

---
+++ src/Auth.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Laiux\Auth;
+
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Client;
+
+class Auth {
+    public static function signIn(string $username, string $password, string $userAgent): string | null{
+
+        $url = config('laiux_auth.laiux_auth_server_url');
+
+        if($url == null) abort(401);
+
+        $url = $url.'/api/auth';
+
+        $headers = [
+            'User-Agent' => $userAgent
+        ];
+
+        try {
+            $client = new Client();
+
+            $response = $client->post($url, [
+                'headers' => $headers,
+                'json' => [
+                    'email' => $username,
+                    'password' => $password
+                ]
+            ]);
+
+            if(!($response->getStatusCode() == 200)) abort(401);
+
+            $responseBody = $response->getBody()->getContents();
+
+            $data = json_decode($responseBody, true);
+
+            return $data;
+
+        } catch (ClientException $e) {
+            if ($e->hasResponse() && $e->getResponse()->getStatusCode() != 200) {
+                abort(401);
+            } else {
+                $responseBody = $e->getResponse()->getBody()->getContents();
+
+                $data = json_decode($responseBody, true);
+
+                if(!(isset($data['success']) && $data['success'] == true)) abort(401);
+
+                return $data;
+            }
+        }
+
+    }
+}
